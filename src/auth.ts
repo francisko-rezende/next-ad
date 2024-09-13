1d88054b7731acb721055b52853a00985dedbab0
Francisko de Moraes Rezende <francisko.rezende@gmail.com>
2024-09-13 08:56:54 -0300

    feat: add azure ad configs to next auth options

--- src/auth.ts
+++ src/auth.ts
@@ -1,5 +1,28 @@
 import NextAuth from "next-auth";
+import AzureAdProvider from "next-auth/providers/azure-ad";
 
-export const { handlers, signIn, signOut, auth } = NextAuth({
-  providers: [],
+export const BASE_PATH = "http://localhost:3000";
+
+export const { handlers, auth, signIn, signOut } = NextAuth({
+  providers: [
+    AzureAdProvider({
+      clientId: process.env.AZURE_AD_CLIENT_ID,
+      clientSecret: process.env.AZURE_AD_CLIENT_SECRET,
+      tenantId: process.env.AZURE_AD_TENANT_ID,
+    }),
+  ],
+  pages: { signIn: "/" },
+  secret: process.env.NEXTAUTH_SECRET,
+  callbacks: {
+    async jwt({ token, account }) {
+      if (account) {
+        token.idToken = account.id_token as string;
+        token.accessToken = account.access_token as string;
+      }
+      return token;
+    },
+    authorized({ auth }) {
+      return !!auth;
+    },
+  },
 });
