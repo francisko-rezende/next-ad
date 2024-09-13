a8f9413764049a6b7be9f4528c380cf1bb95e7d3
Francisko de Moraes Rezende <francisko.rezende@gmail.com>
2024-09-13 09:00:02 -0300

    chore: delete commented code

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,26 +1,7 @@
-// import { NextResponse } from "next/server";
-// import { auth, BASE_PATH } from "@/auth";
-//
-// export const config = {
-//   matcher: ["/((?!api|_next/static|_next/image|favicon.ico).*)"],
-// };
-//
-// export default auth((req) => {
-//   console.log("oi");
-//   const reqUrl = new URL(req.url);
-//   if (!req.auth && reqUrl?.pathname !== "/") {
-//     return NextResponse.redirect(new URL(BASE_PATH));
-//   }
-// });
-//
-
 import { auth } from "@/auth";
 
 export default auth((req) => {
-  // console.log(req.nextUrl);
-  // console.log(req.auth);
   if (!req.auth && req.nextUrl.pathname !== "/") {
-    console.log("dentro");
     const newUrl = new URL("/", req.nextUrl.origin);
     return Response.redirect(newUrl);
   }
